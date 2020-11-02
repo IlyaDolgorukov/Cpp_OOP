62ec58d25f819489d012f52f40c23710552225ec
Ilya Dolgorukov <web@difabrik.ru>
2020-11-02 17:55:32 +0500

    homework3

---
+++ homework3/04.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+enum Suit {spades, hearts, clubs, diamonds};
+enum Val {ace, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king};
+
+class Card {
+private:
+    Val val;
+    Suit suit;
+    bool position;
+public:
+    Card(Val v, Suit s, bool p) : val(v), suit(s), position(p) {}
+    void Flip() {
+        position = !position;
+    }
+    int GetValue() {
+        return val < 10 ? val + 1 : 10;
+    }
+};
+
+int main() {
+    Card card(ace, diamonds, false);
+    cout << "Value is " << card.GetValue() << endl;
+
+    return 0;
+}
